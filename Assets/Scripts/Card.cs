7bb858a27bab42ad37dabb5deabaac7190267e62
VN <vnguyen1446@gmail.com>
2023-09-09 12:05:24 -0700

    added scriptable objects

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int attack;
+    public int defense;
+    public int duration; // for special cards. E.g buff the player attack 
+    public int manaCost;
+}
